4d4cabd5d258dd59c1dd06b3780aea1f7668ccc2
Andres Sepulveda <AndresSepulveda@users.noreply.github.com>
2020-09-28 19:02:20 -0300

    Update gradient_temp.m

--- gradient_temp.m
+++ gradient_temp.m
@@ -2,8 +2,8 @@ close all
 clear al
 
 %
-% ncra -d time,1,31 temp_dgeo_2018.nc temp_ene_2018.nc
-% Feb 32,59, Mar 60 90, Apr  91 102, May  121 151,  Jun 152 181, Jul 182 212,
+% ncra -F -d time,1,31 temp_dgeo_2018.nc temp_ene_2018.nc
+% Feb 32,59, Mar 60 90, Apr  91 120, May  121 151,  Jun 152 181, Jul 182 212,
 % Ago 213 243, Sep 244 273, Oct  274 304, Sep  244 273, Oct  274 304, Nov 305 334, Dec  335 365
 
 nc=netcdf('temp_ene_2018.nc','r');
