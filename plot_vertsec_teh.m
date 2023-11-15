489ae7f6417f5bca0e7f73de375316980c3f1103
Andres Sepulveda <AndresSepulveda@users.noreply.github.com>
2023-11-15 15:55:13 -0300

    Update plot_vertsec_teh.m

--- plot_vertsec_teh.m
+++ plot_vertsec_teh.m
@@ -21,30 +21,34 @@ fecha_base = datetime(2000, 1, 1);
 
 c_fecha = datetime(c_yr, c_mes, c_dia);
 c_s2000 = seconds(c_fecha - fecha_base);
-min(c_s2000)
-max(c_s2000)
+%min(c_s2000)
+%max(c_s2000)
 
 ac_fecha = datetime(ac_yr, ac_mes, ac_dia);
 ac_s2000 = seconds(ac_fecha - fecha_base);
 
 %hisfile='crocT_his_Y2001M09.nc';
-hisfile='crocT_avg_Y2000M09.nc';
+hisfile09='crocT_avg_Y2000M09.nc';
+hisfile10='crocT_avg_Y2000M10.nc';
 gridfile='croco_grd.nc';
 
-nh=netcdf(hisfile,'r');
-tiempo=nh{'time'}(:);
+nh09=netcdf(hisfile09,'r');
+tiempo09=nh09{'time'}(:);
+nh10=netcdf(hisfile10,'r');
+tiempo10=nh10{'time'}(:);
 
-for i = 1:1 % length(c_s2000)
-    [~, tindex] = min(abs(tiempo - c_s2000(i)));
-    tindex
-    min(tiempo)
-    max(tiempo)
-    c_s2000(i)
+for i = 1:3 % length(c_s2000)
     figure(i)
-    vertslice(hisfile,gridfile,[c_lon(i)+0.25 c_lon(i)-0.25],[c_lat(i)+0.25 c_lat(i)-0.25],'temp',tindex,1,10,28,10,-400,10) %,...
-%                   1,20,25,0,10,-200)
-%    vertslice(hisfile,gridfile,[c_lon+0.5 c_lon-0.5],[c_lat+0.5 c_lat-0.5],'temp',tindex,...
-%                   coef,colmin,colmax,ncol,zmin,zmax,xmin,xmax,...
-%		   pltstyle,h0,handles,Yorig)
+    c_fecha(i)
+    [~, tindex] = min(abs(tiempo09 - c_s2000(i)));
+    vertslice(hisfile09,gridfile,[c_lon(i)+0.25 c_lon(i)-0.25], ...
+      [c_lat(i)+0.25 c_lat(i)-0.25],'temp',tindex,1,10,28,10,-400,10)
+end
 
+for i = 4:length(c_s2000)
+    figure(i)
+    c_fecha(i)
+    [~, tindex] = min(abs(tiempo10 - c_s2000(i)));
+    vertslice(hisfile10,gridfile,[c_lon(i)+0.25 c_lon(i)-0.25], ...
+      [c_lat(i)+0.25 c_lat(i)-0.25],'temp',tindex,1,10,28,10,-400,10)
 end
